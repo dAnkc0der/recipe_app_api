5a2a57c35fefebe465225544eac42af4395a0b91
dAnkc0der <utkarshverma007@outlook.com>
2022-04-27 19:02:06 +0530

    Edited user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, \
+                                        PermissionsMixin
 
-# Create your models here.
 
 class UserManager(BaseUserManager):
 
@@ -23,6 +23,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
